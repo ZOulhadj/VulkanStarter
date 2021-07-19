d05a2ac831951d01372420caf013bf0ae7837f0f
ZOulhadj <zakariyaoulhadj01@gmail.com>
2021-07-19 23:03:31 +0200

    Small changes.

--- CompileShaders.sh
+++ CompileShaders.sh
@@ -1,9 +1,11 @@
+
+# location to the SPIR-V compiler within the Vulkan SDK
 glslCompilerLocation="../../VulkanSDK/1.2.182.0/macOS/bin/glslc"
 
+# the text shader source file locations
 vertexShaderSourceLocation="Source/Shaders/Triangle.vert"
 fragmentShaderSourceLocation="Source/Shaders/Triangle.frag"
 
-shaderOutputFolder="Build/"
-
+# compile the vertex and fragment shaders
 $glslCompilerLocation $vertexShaderSourceLocation -o Build/Vert.spv
 $glslCompilerLocation $fragmentShaderSourceLocation -o Build/Frag.spv
